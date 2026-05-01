af152e365b69325d043f3f1162cc747ff567ac42
Ammar <dev.ammar15@gmail.com>
2026-05-01 21:06:10 +0500

    Removed errors

--- todo-frontend/eslint.config.mjs
+++ todo-frontend/eslint.config.mjs
@@ -1,13 +1,18 @@
 import js from "@eslint/js";
-import reactPlugin from "eslint-plugin-react";
+import globals from "globals";
+
 export default [
-	js.configs.recommended,
-	reactPlugin.configs.flat.recommended,
 	{
-		settings: { react: { version: "detect" } },
+		ignores: ["eslint.config.mjs"], // don't lint the config file itself
+	},
+	js.configs.recommended,
+	{
 		languageOptions: {
 			ecmaVersion: 2021,
 			sourceType: "module",
+			globals: {
+				...globals.browser,
+			},
 		},
 	},
 ];
--- todo-frontend/src/util/apiClient.js
+++ todo-frontend/src/util/apiClient.js
@@ -3,10 +3,7 @@ import axios from "axios";
 // const VITE_BACKEND_URL = "http://localhost:3000/"
 
 const apiClient = axios.create({
-  baseURL:
-    import.meta.env.VITE_BACKEND_URL ||
-    "http://localhost:3000/" ||
-    "http://backend:3000/",
+	baseURL: import.meta.env.VITE_BACKEND_URL || "http://localhost:3000/",
 });
 
 export default apiClient;
